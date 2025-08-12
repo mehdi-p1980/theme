37fb976d4b878aae59854ae9bb0d5f1ed377e588
DevSmart <mr.mehdi.p@gmail.com>
2025-08-12 16:24:26 +0330

    Add files via upload

---
+++ Themes/LeptonXLite/PublicLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Components.Web.LeptonXLiteTheme.Themes.LeptonXLite
+{
+    public partial class PublicLayout : IDisposable
+    {
+        [Inject] IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("initLeptonX");
+            }
+        }
+
+        public void Dispose()
+        {
+            //NavigationManager.LocationChanged -= OnLocationChanged;
+        }
+
+    }
+}
