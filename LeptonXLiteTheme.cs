dd3e9975cbe880177cad2eb137d57ab97c88bf43
DevSmart <mr.mehdi.p@gmail.com>
2025-08-12 16:25:01 +0330

    Add files via upload

---
+++ LeptonXLiteTheme.cs
@@ -0,0 +1,28 @@
+using System;
+using Volo.Abp.AspNetCore.Components.Web.LeptonXLiteTheme.Themes.LeptonXLite;
+using Volo.Abp.AspNetCore.Components.Web.Theming.Layout;
+using Volo.Abp.AspNetCore.Components.Web.Theming.Theming;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Components.Web.LeptonXLiteTheme;
+
+[ThemeName(Name)]
+public class LeptonXLiteTheme : ITheme, ITransientDependency
+{
+    public const string Name = "LeptonXLite";
+    
+    public Type GetLayout(string name, bool fallbackToDefault = true)
+    {
+        switch (name)
+        {
+            case StandardLayouts.Public:
+                return typeof(PublicLayout);
+            case StandardLayouts.Application:
+            case StandardLayouts.Account:
+            case StandardLayouts.Empty:
+                return typeof(MainLayout);
+            default:
+                return fallbackToDefault ? typeof(MainLayout) : typeof(NullLayout);
+        }
+    }
+}
